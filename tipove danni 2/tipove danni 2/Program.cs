c4e35974c729287eb76eb8012598b7bb88596b46
Alito41 <99259014+Alito41@users.noreply.github.com>
2022-09-26 09:36:58 +0300

    Add files via upload

---
+++ tipove danni 2/tipove danni 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace tipove_danni_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char bukva = 'C';
+            sbyte number = 100;
+            short chislo = 20000;
+            int u2 = 300000;
+            string prog = "Programirane";
+            Console.WriteLine("Promenlivata ot tip char e: "+bukva);
+            Console.WriteLine();
+            Console.WriteLine("Promenlivata ot tip sbyte e: "+number);
+            Console.WriteLine();
+            Console.WriteLine("Promenlivata ot tip short e: "+chislo);
+            Console.WriteLine();
+            Console.WriteLine("Promenlivata ot tip int e: " +u2);
+            Console.WriteLine();
+            Console.WriteLine("Promenlivata ot tip string e: " +prog);
+            Console.WriteLine();
+        }
+    }
+}
